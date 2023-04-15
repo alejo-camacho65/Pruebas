77b5cf27b75dc72be5d8930f713891ecff48a118
Diego Alejandro Camacho Vargas <Dcamacho@intergrupo.com>
2023-04-15 07:32:06 -0500

    Punto 17. Division de dos numeros.

---
+++ Pruebas/NumeroEnteros.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Pruebas
+{
+    public class NumeroEnteros
+    {
+        public int Division(int numeroUno, int numeroDos)
+        {
+            int resultado = numeroUno / numeroDos;
+            return resultado;
+        }
+    }
+}
--- Pruebas/Program.cs
+++ Pruebas/Program.cs
@@ -6,7 +6,14 @@ namespace Pruebas
     {
         static void Main(string[] args)
         {
-           
+            Console.WriteLine("Escriba el primer numero: ");
+            int numeroUno = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Escriba el segundo numero: ");
+            int numeroDos = Convert.ToInt32(Console.ReadLine());
+
+            NumeroEnteros numeroEnteros = new NumeroEnteros();
+            var resultado = numeroEnteros.Division(numeroUno, numeroDos);
+            Console.WriteLine(resultado);
         }
     }
 }
