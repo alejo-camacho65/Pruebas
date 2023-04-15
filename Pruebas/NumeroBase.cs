18f8e9077019012a3accacec5791272960e02f2f
Diego Alejandro Camacho Vargas <Dcamacho@intergrupo.com>
2023-04-15 08:04:19 -0500

    Punto 18. Cambio de base 10

---
+++ Pruebas/NumeroBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Pruebas
+{
+    public class NumeroBase
+    {
+        public string ObtenerCambioBase(int numeroUno, int numeroBase)
+        {
+            int division = numeroUno / numeroBase;
+            int residuo = numeroUno % numeroBase;
+            string resultado = Convert.ToString(residuo);
+
+            while (division != 1)
+            {
+                residuo = division % numeroBase;
+                division = division / numeroBase;
+
+                resultado = residuo + resultado;
+            }
+            resultado = division + "" + resultado;
+            return resultado;
+        }
+    }
+}
--- Pruebas/Program.cs
+++ Pruebas/Program.cs
@@ -6,8 +6,14 @@ namespace Pruebas
     {
         static void Main(string[] args)
         {
-           
-        }
+            Console.WriteLine("Escriba elnumero: ");
+            int numeroUno = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Escriba la base: ");
+            int numeroBase = Convert.ToInt32(Console.ReadLine());
+
+            NumeroBase numeroBases = new NumeroBase();
+            string resultado = numeroBases.ObtenerCambioBase(numeroUno, numeroBase);
+            Console.WriteLine("El numero " + numeroUno + " B10 = " + resultado + " B" + numeroBase);
+        }        
     }
 }
-
